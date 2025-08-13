17064633bea5f996631087769a75c9214931eec6
freelanceradnan <reactorbro722@gmail.com>
2025-08-13 17:33:30 +0600

    methods number added in js

--- methods.js
+++ methods.js
@@ -1,24 +1,24 @@
 // //parse integer methods return booleand value
-// let res=Number.parseInt('10')
-// console.log(res)
+let res=Number.parseInt('10')
+console.log(res)
 
 // //value not a number with parse int
-// let res=Number.parseInt('adnan')
-// console.log(res)
+let res=Number.parseInt('adnan')
+console.log(res)
 
 // //parsefloat methods prints with points
-// let res=Number.parseFloat(10.12)
-// console.log(res)
+let res=Number.parseFloat(10.12)
+console.log(res)
 
 // //parsefloat methods prints with negative 
-// let res=Number.parseFloat(-10)
-// console.log(res)
+let res=Number.parseFloat(-10)
+console.log(res)
 
 
 // //tofixed methods
-// let num=10.23665;
-// let res=num.toFixed(2)
-// console.log(res)
+let num=10.23665;
+let res=num.toFixed(2)
+console.log(res)
 
 //toString methods
 
