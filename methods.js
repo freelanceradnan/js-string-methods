518bbed720c225e176905087d2e2523e488e0aec
freelanceradnan <reactorbro722@gmail.com>
2025-08-13 17:31:42 +0600

    tostring number methods added

--- methods.js
+++ methods.js
@@ -15,7 +15,21 @@
 // console.log(res)
 
 
-//tofixed methods
-let num=10.23665;
-let res=num.toFixed(2)
+// //tofixed methods
+// let num=10.23665;
+// let res=num.toFixed(2)
+// console.log(res)
+
+//toString methods
+
+let num=10;
+let res=num.toString(2);//Base 2 is binary
+console.log(res)
+
+let num=10;
+let res=num.toString(8);//Base 8 is octal
+console.log(res)
+
+let num=10;
+let res=num.toString(2);//Base 16 is hexadecimal
 console.log(res)
