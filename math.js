7a4b16a6f5ebe32dc04929d2e9ae4df7e95daeb8
freelanceradnan <reactorbro722@gmail.com>
2025-08-13 17:48:59 +0600

    math methos in js added

--- math.js
+++ math.js
@@ -1,23 +1,39 @@
 // //abs return absolute value of numbers
-// let a=Math.abs(10)
-// console.log(a)
+let a=Math.abs(10)
+console.log(a)
 
-// let a=Math.abs(-10)
-// console.log(a)
+let a=Math.abs(-10)
+console.log(a)
 
-// let a=Math.abs(10.12100)
-// console.log(a)
+let a=Math.abs(10.12100)
+console.log(a)
 
 // //js ceil
-// let a=Math.ceil(1.2)
-// console.log(a)
+let a=Math.ceil(1.2)
+console.log(a)
 
-// let b=Math.ceil(2.1)
-// console.log(b)
+let b=Math.ceil(2.1)
+console.log(b)
 
-// //floor in js
+// // //floor in js
 let a=Math.floor(2.1)
 console.log(a)
 
 let a=Math.floor(3.1)
+console.log(a)
+
+// //random
+let a=Math.random()
+console.log(a)
+
+// //power methods
+let a=Math.pow(20,2)
+console.log(a)
+
+// //max methods
+let a=Math.max(5,23,3412,32,12)
+console.log(a)
+
+//min methods
+let a=Math.min(2,1,33,44,453)
 console.log(a)
