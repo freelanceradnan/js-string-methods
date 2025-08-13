6bf36ef785af818a159432dd7b189bc7fd78a466
freelanceradnan <reactorbro722@gmail.com>
2025-08-13 17:41:18 +0600

    math methods added with abs

---
+++ math.js
@@ -0,0 +1,9 @@
+//abs return absolute value of numbers
+let a=Math.abs(10)
+console.log(a)
+
+let a=Math.abs(-10)
+console.log(a)
+
+let a=Math.abs(10.12100)
+console.log(a)
--- methods.js
+++ methods.js
@@ -1,35 +1,35 @@
-// //parse integer methods return booleand value
-let res=Number.parseInt('10')
-console.log(res)
+// // //parse integer methods return booleand value
+// let res=Number.parseInt('10')
+// console.log(res)
 
-// //value not a number with parse int
-let res=Number.parseInt('adnan')
-console.log(res)
+// // //value not a number with parse int
+// let res=Number.parseInt('adnan')
+// console.log(res)
 
-// //parsefloat methods prints with points
-let res=Number.parseFloat(10.12)
-console.log(res)
+// // //parsefloat methods prints with points
+// let res=Number.parseFloat(10.12)
+// console.log(res)
 
-// //parsefloat methods prints with negative 
-let res=Number.parseFloat(-10)
-console.log(res)
+// // //parsefloat methods prints with negative 
+// let res=Number.parseFloat(-10)
+// console.log(res)
 
 
-// //tofixed methods
-let num=10.23665;
-let res=num.toFixed(2)
-console.log(res)
+// // //tofixed methods
+// let num=10.23665;
+// let res=num.toFixed(2)
+// console.log(res)
 
-//toString methods
+// //toString methods
 
-let num=10;
-let res=num.toString(2);//Base 2 is binary
-console.log(res)
+// let num=10;
+// let res=num.toString(2);//Base 2 is binary
+// console.log(res)
 
-let num=10;
-let res=num.toString(8);//Base 8 is octal
-console.log(res)
+// let num=10;
+// let res=num.toString(8);//Base 8 is octal
+// console.log(res)
 
-let num=10;
-let res=num.toString(2);//Base 16 is hexadecimal
-console.log(res)
+// let num=10;
+// let res=num.toString(2);//Base 16 is hexadecimal
+// console.log(res)
