89714c6af86bd41a0ba4c7cc1d50d1b033573473
freelanceradnan <reactorbro722@gmail.com>
2025-08-13 17:43:00 +0600

    ceil js added

--- math.js
+++ math.js
@@ -1,9 +1,16 @@
-//abs return absolute value of numbers
-let a=Math.abs(10)
+// //abs return absolute value of numbers
+// let a=Math.abs(10)
+// console.log(a)
+
+// let a=Math.abs(-10)
+// console.log(a)
+
+// let a=Math.abs(10.12100)
+// console.log(a)
+
+//js ceil
+let a=Math.ceil(1.2)
 console.log(a)
 
-let a=Math.abs(-10)
-console.log(a)
-
-let a=Math.abs(10.12100)
-console.log(a)
+let b=Math.ceil(2.1)
+console.log(b)
