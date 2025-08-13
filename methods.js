a9389c1f36556bada4ff7460326bb22beb6bb540
freelanceradnan <reactorbro722@gmail.com>
2025-08-13 17:25:46 +0600

    parsefloat value added with negative

--- methods.js
+++ methods.js
@@ -1,7 +1,15 @@
-//parse integer methods return booleand value
-let res=Number.parseInt('10')
-console.log(res)
+// //parse integer methods return booleand value
+// let res=Number.parseInt('10')
+// console.log(res)
 
-//value not a number with parse int
-let res=Number.parseInt('adnan')
+// //value not a number with parse int
+// let res=Number.parseInt('adnan')
+// console.log(res)
+
+// //parsefloat methods prints with points
+// let res=Number.parseFloat(10.12)
+// console.log(res)
+
+//parsefloat methods prints with negative 
+let res=Number.parseFloat(-10)
 console.log(res)
