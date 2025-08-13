39dcdc615a40388fd9c61745418d31aa689513e5
freelanceradnan <reactorbro722@gmail.com>
2025-08-13 17:22:14 +0600

    adding all methods.js files

---
+++ methods.js
@@ -0,0 +1,7 @@
+//parse integer methods return booleand value
+let res=Number.parseInt('10')
+console.log(res)
+
+//value not a number with parse int
+let res=Number.parseInt('adnan')
+console.log(res)
